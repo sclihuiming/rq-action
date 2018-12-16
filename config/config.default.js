bdc469e5491a553fe22085c547ea8e121d1a0d60
lihuiming <sclihuiming@163.com>
2018-12-16 12:47:03 +0800

    init plugin

--- config/config.default.js
+++ config/config.default.js
@@ -9,5 +9,29 @@ module.exports = appInfo => {
   // add your config here
   config.middleware = [];
 
+  config.redis = {
+    client: {
+      port: 6379,          // Redis port
+      host: '127.0.0.1',   // Redis host
+      password: '',
+      db: 0,
+    },
+  }
+  //socket.io config
+  config.io = {
+    init: { }, // passed to engine.io
+    namespace: {
+      '/': {
+        connectionMiddleware: [],
+        packetMiddleware: [],
+      },
+    },
+    redis: {
+      host: '127.0.0.1',
+      port: 6379
+    }
+  };
+  
+
   return config;
 };
--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,37 @@
 
 // had enabled by egg
 // exports.static = true;
+exports.redis = {
+  enable: true,
+  package: 'egg-redis',
+};
+
+exports.validate = {
+  enable: true,
+  package: 'egg-validate',
+};
+
+exports.io = {
+  enable: true,
+  package: 'egg-socket.io',
+};
+
+exports.mongoose = {
+  enable: true,
+  package: 'egg-mongoose',
+};
+
+exports.graphql = {
+  enable: true,
+  package: 'egg-graphql',
+};
+
+exports.cors = {
+  enable: true,
+  package: 'egg-cors',
+};
+
+exports.oss = {
+  enable: true,
+  package: 'egg-oss',
+};
